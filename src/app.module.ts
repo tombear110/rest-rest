6a2fecb8b24079a1aa0b31001ca22a16d673ef24
tangte <tangte@sumztech.com>
2020-11-11 15:12:54 +0800

    feat: inti

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+// app.module.ts
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './server/user/user.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/MongoDB'), UserModule],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
